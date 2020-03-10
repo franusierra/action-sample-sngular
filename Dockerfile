7624352b03d0434ba503bc780929644c5f2d4d75
franusierra <franusierra@gmail.com>
2020-03-10 10:13:02 +0100

    feat: add workflow file

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,4 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 8080
 CMD [ "node", "hola-gente.js" ]
--- hola-gente.js
+++ hola-gente.js
@@ -1,3 +1,4 @@
 'use strict';
-var args=procces.argv.slice(2);
-console.log(`La accion ha sido ejecutada con exito, ¡Hola ${args[0]} ✌!`);
+var args=process.argv.slice(2);
+
+console.log(`La accion ha sido ejecutada con exito, ¡Hola ${args[0]?args[0]:'gente'} ✌!`);
